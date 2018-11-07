7a0589961ba853debd3ab3d281f38b6005a3f65a
Tyler Long <tyler4long@gmail.com>
2018-11-07 17:48:57 +0800

    bot setupWebHook

--- src/handlers/bot.js
+++ src/handlers/bot.js
@@ -1,13 +1,30 @@
+import delay from 'timeout-as-promise'
+
 import Bot from '../models/Bot'
 
 const handle = app => {
   // add bot to Glip
   app.get('/bot-oauth', async (req, res) => {
-    await Bot.authorize(req.query.code)
+    const bot = await Bot.authorize(req.query.code)
     res.send('Bot added')
+
+    // setup WebHook
+    let done = false
+    while (!done) { // cannot setup WebHook until bot user is ready
+      await delay(10000)
+      done = bot.setupWebHook()
+    }
+  })
+
+  // notification bot for bot users
+  app.post('/bot-webhook', async (req, res) => {
+    const message = req.body
+    console.log('Message received via bot WebHook:', JSON.stringify(message, null, 2))
+    res.header('validation-token', req.header('validation-token'))
+    res.send('/bot-webhook replied')
   })
 }
 
-const bot = { handle }
+const botHandler = { handle }
 
-export default bot
+export default botHandler
--- src/models/Bot.js
+++ src/models/Bot.js
@@ -26,7 +26,7 @@ Bot.authorize = async code => {
     throw e
   }
   const token = rc.token()
-  Bot.create({
+  return Bot.create({
     id: token.owner_id,
     token
   })
@@ -56,6 +56,31 @@ Bot.prototype.validate = async function () {
       return false
     }
     console.log('Bot validate', e.response.data)
+    throw e
+  }
+}
+
+Bot.prototype.setupWebHook = async function () {
+  try {
+    await this.rc.post('/restapi/v1.0/subscription', {
+      eventFilters: [
+        '/restapi/v1.0/glip/posts',
+        '/restapi/v1.0/glip/groups',
+        '/restapi/v1.0/account/~/extension/~'
+      ],
+      expiresIn: 473040000, // 15 years
+      deliveryMode: {
+        transportType: 'WebHook',
+        address: process.env.RINGCENTRAL_CHATBOT_SERVER + '/bot-webhook'
+      }
+    })
+    return true
+  } catch (e) {
+    const errorCode = e.response.data.errorCode
+    if (errorCode === 'SUB-406') {
+      return false
+    }
+    console.log('Bot setupWebHook', e.response.data)
     throw e
   }
 }
