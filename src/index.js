c52333c5278f9ec99e4d34a22f3fbe1f976931db
Tyler Long <tyler4long@gmail.com>
2018-11-10 20:42:57 +0800

    RINGCENTRAL_CHATBOT_EXPRESS_PORT

--- src/index.js
+++ src/index.js
@@ -15,9 +15,10 @@ if (process.env.LAMBDA_TASK_ROOT) { // AWS Lambda
   const server = serverlessExpress.createServer(app)
   exports.main = (event, context) => serverlessExpress.proxy(server, event, context)
 } else {
-  app.listen(3000, async () => {
-    console.log(`Listening on 3000`)
-    await request.put('http://localhost:3000/admin/setup-database')
-    await request.put('http://localhost:3000/admin/reboot')
+  const port = process.env.RINGCENTRAL_CHATBOT_EXPRESS_PORT
+  app.listen(port, async () => {
+    console.log(`Listening on ${port}`)
+    await request.put(`http://localhost:${port}/admin/setup-database`)
+    await request.put(`http://localhost:${port}/admin/reboot`)
   })
 }
