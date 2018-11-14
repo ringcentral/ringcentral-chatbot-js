10b760155a9cbde76b1cb5be0c357b617056873e
Tyler Long <tyler4long@gmail.com>
2018-11-14 20:34:22 +0800

    Refactor code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import serverlessHTTP from 'serverless-http'
+import Lambda from 'aws-sdk/clients/lambda'
 
 import botApp from './apps/bot'
 import adminApp from './apps/admin'
@@ -12,24 +13,19 @@ app.use('/bot', botApp)
 
 const port = process.env.RINGCENTRAL_CHATBOT_EXPRESS_PORT
 if (port) { // express.js
-  app.listen(port, async () => {
-    console.log(`Listening on ${port}`)
-    const request = require('request-promise')
-    await request.put(`http://localhost:${port}/admin/setup-database`)
-    await request.put(`http://localhost:${port}/admin/reboot`)
-  })
+  app.listen(port)
 } else { // AWS lambda
   module.exports.handler = serverlessHTTP(app)
   module.exports.proxy = (event, context, callback) => {
-    console.log(JSON.stringify(event, null, 2))
-    const Lambda = require('aws-sdk/clients/lambda')
     const lambda = new Lambda({ region: process.env.AWS_REGION })
     lambda.invoke({
       FunctionName: process.env.AWS_LAMBDA_FUNCTION_NAME.replace(/-proxy$/, '-app'),
       InvocationType: 'Event', // so `lambda.invoke` is async
       Payload: JSON.stringify(event)
     }, (error, data) => {
-      console.log(error, data)
+      if (error) {
+        console.log(error, data)
+      }
     })
     callback(null, { statusCode: 200, body: '', headers: { 'Validation-Token': event.headers['Validation-Token'] } })
   }
