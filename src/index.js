503bfa9bbe7ff48179f609f55292fbd798d8915a
Tyler Long <tyler4long@gmail.com>
2018-11-15 21:44:37 +0800

    Crontab

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import express from 'express'
 import serverlessHTTP from 'serverless-http'
 import Lambda from 'aws-sdk/clients/lambda'
+import cronParser from 'cron-parser'
+import moment from 'moment-timezone'
 
 import botApp from './apps/bot'
 import adminApp from './apps/admin'
+import Service from './models/Service'
+import Bot from './models/Bot'
 
 const app = express()
 app.use(express.json())
@@ -28,5 +32,20 @@ if (port) { // express.js
       }
     })
     callback(null, { statusCode: 200, body: '', headers: { 'Validation-Token': event.headers['Validation-Token'] } })
+  }
+  module.exports.crontab = async (event, content, callback) => {
+    const services = await Service.findAll({
+      name: 'Crontab'
+    })
+    for (const service of services) {
+      const interval = cronParser.parseExpression(service.data.expression, { utc: true })
+      const prevTimestamp = interval.prev()._date
+      const currentTimestamp = moment.tz(new Date(), 'utc')
+      const delta = currentTimestamp - prevTimestamp
+      if (delta < 30) {
+        const bot = await Bot.findByPk(service.botId)
+        bot.sendMessage(service.groupId, { text: service.message })
+      }
+    }
   }
 }
