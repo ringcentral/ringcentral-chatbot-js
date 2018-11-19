a579b492dba431281c2016714f96304c40726190
Tyler Long <tyler4long@gmail.com>
2018-11-19 15:05:16 +0800

    Minor change

--- src/apps/bot.js
+++ src/apps/bot.js
@@ -35,7 +35,7 @@ const createApp = handle => {
         default:
           break
       }
-      await handle({ type: `WebHook_Event_${body.eventType}`, message })
+      await handle({ type: body.eventType, message })
     }
     res.header('Validation-Token', req.header('Validation-Token'))
     res.send('')
