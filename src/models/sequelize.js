8a3566449a8378d83a6ef3e0768cc821f055e4ae
Tyler Long <tyler4long@gmail.com>
2019-03-18 14:59:41 +0800

    Upgrade dependencies

--- src/models/sequelize.js
+++ src/models/sequelize.js
@@ -3,7 +3,6 @@ import Sequelize from 'sequelize'
 const sequelize = new Sequelize(
   process.env.RINGCENTRAL_CHATBOT_DATABASE_CONNECTION_URI,
   {
-    operatorsAliases: false,
     define: {
       timestamps: true
     },
