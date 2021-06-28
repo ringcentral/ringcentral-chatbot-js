1c09e56b497645667fd5e4835a0fdea8a41b3bf5
Tyler Long <tyler4long@gmail.com>
2021-06-28 16:22:10 -0700

    convert lambda.js to lambda.ts

---
+++ src/lambda.ts
@@ -0,0 +1,40 @@
+import Lambda from 'aws-sdk/clients/lambda';
+import request from 'supertest';
+
+export const createAsyncProxy = (functionName: string, filterApp: any) => {
+  const lambda = new Lambda({region: process.env.AWS_REGION});
+  return async (event: any) => {
+    const lambdaFunction = async () => {
+      await lambda
+        .invoke({
+          FunctionName: process.env.AWS_LAMBDA_FUNCTION_NAME!.replace(
+            /-proxy$/,
+            `-${functionName}`
+          ),
+          InvocationType: 'Event',
+          Payload: JSON.stringify(event),
+        })
+        .promise();
+      return {
+        statusCode: 200,
+        headers: {
+          'Validation-Token': event.headers['Validation-Token'],
+          'Content-Type': 'text/html',
+        },
+        body: '<!doctype><html><body><script>close()</script><p>Please close this page</p></body></html>',
+      };
+    };
+    if (!filterApp) {
+      return lambdaFunction();
+    }
+    const response = await request(filterApp).get(event.path);
+    if (response.statusCode === 404) {
+      return lambdaFunction();
+    }
+    return {
+      statusCode: response.statusCode,
+      headers: response.headers,
+      body: response.text,
+    };
+  };
+};
